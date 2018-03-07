61a4bc6232aae0346d4889d700b99273ce1719f2
gabr1e11 <gabr1e11@hotmail.com>
2018-03-07 14:48:06 +0100

    Updated script to support more than 1 app

--- throttle_xcode.sh
+++ throttle_xcode.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
 
+# Add to this array all the apps you want to throttle down
+declare -a PROCESS_NAME_LIST=("com.apple.dt.SKAgent")
+
 # Defaults
+ROOT_PROCESS_PID=
 CPU_THROTTLE_PERCENT=50
-PREV_SKAGENT_PID=
+PREV_PROCESS_PID=
 
 function main()
 {
+    trap_ctrl_c
+
+    ROOT_PROCESS_PID=$(get_current_process_pid)
+
     if [ "`is_sudo_active`" == "0" ]
     then
         echo "Please, rerun this script with sudo"
         exit 1
     fi
 
-    parse_input_params
+    parse_input_params $@
 
-    while :
+    for PROCESS_NAME in "${PROCESS_NAME_LIST[@]}"
     do
-        SKAGENT_PID=$(getSKAgentPID)
-
-        # Wait for the process to start
-        while [ -z $SKAGENT_PID ] || [ "$PREV_SKAGENT_PID" == "$SKAGENT_PID" ]
-        do
-            sleep 5
-            SKAGENT_PID=$(getSKAgentPID)
-        done
-
-        # Throttle it down
-        echo "-> cputhrottle $SKAGENT_PID $CPU_THROTTLE_PERCENT"
-        cputhrottle $SKAGENT_PID $CPU_THROTTLE_PERCENT &
-
-        PREV_SKAGENT_PID=$SKAGENT_PID
+        ( throttle_process $PROCESS_NAME $CPU_THROTTLE_PERCENT ) &
     done
+
+    # Wait for the subprocesses to finish
+    wait
+
+    echo "Done!"
 }
 
 # Helper functions
+function trap_ctrl_c()
+{
+    # Kills all processes in the process group
+    trap "kill 0" SIGINT
+}
+
+function ctrl_c_handler() {
+    echo "** Trapped CTRL-C"
+
+    CHILD_PROCESSES_PIDS=$(ps -o pid,ppid -ax | awk "{ if ( \$2 == $ROOT_PROCESS_PID) { print \$1 }}")
+
+    for CHILD_PID in $CHILD_PROCESSES_PIDS
+    do
+        kill -9 $CHILD_PID 2>/dev/null
+    done
+}
+
 function is_sudo_active()
 {
     if [ "`whoami`" == "root" ]
@@ -46,14 +63,14 @@ function is_sudo_active()
 
 function parse_input_params()
 {
-    while getopts ":c:" option;
+    while getopts ":c:" OPTION
     do
-        case $option in
+        case $OPTION in
             c)
                 CPU_THROTTLE_PERCENT=$OPTARG
                 ;;
             *)
-                usage
+                usage $0
                 exit 1
                 ;;
         esac
@@ -68,10 +85,49 @@ function usage()
     echo ""
 }
 
-function getSKAgentPID()
+function get_current_process_pid()
 {
-    echo `ps -ef | grep com.apple.dt.SKAgent | grep -v grep | tr -s " " | cut -d' ' -f3`
+    echo $$
+}
+
+function get_process_pids()
+{
+    PROCESS_NAME=$1
+    PIDS=`ps -ef | grep "$PROCESS_NAME" | grep -v grep | tr -s " " | cut -d' ' -f3`
+    echo "$PIDS"
+}
+
+function throttle_process()
+{
+    PROCESS_NAME=$1
+    CPU_THROTTLE_PERCENT=$2
+
+    PROCESS_PID=
+    PREV_PROCESS_PID=
+
+    while :
+    do
+        PROCESS_PIDS=$(get_process_pids $PROCESS_NAME)
+
+        # Wait for the process to start
+        while [ -z "$PROCESS_PIDS" ] || [ "$PREV_PROCESS_PIDS" == "$PROCESS_PIDS" ]
+        do
+            sleep 5
+            PROCESS_PIDS=$(get_process_pids $PROCESS_NAME)
+        done
+
+        # Throttle it down
+        for PID in $PROCESS_PIDS
+        do
+            echo "[$PROCESS_NAME] -> cputhrottle $PID $CPU_THROTTLE_PERCENT"
+            cputhrottle $PID $CPU_THROTTLE_PERCENT &
+        done
+
+        PREV_PROCESS_PIDS=$PROCESS_PIDS
+    done
+
+    echo "DONE"
 }
 
 # Call main
-main
+main $@
